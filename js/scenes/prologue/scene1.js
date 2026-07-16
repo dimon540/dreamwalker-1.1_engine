1e9bd55b72cec4bf62af056be7b27d771eb898ea
dimon540 <dmitrozdanevic22@gmail.com>
2026-07-16 14:25:59 +0300

    Add prologue scene 1 with dialogue and background

--- js/scenes/prologue/scene1.js
+++ js/scenes/prologue/scene1.js
@@ -1,1 +1,87 @@
+// =========================
+// DREAMWALKER PROLOGUE SCENE 1
+// =========================
 
+
+console.log("Scene1 завантажено");
+
+
+
+window.scenes = {
+
+
+
+    scene1:{
+
+
+        steps:[
+
+
+            {
+
+
+                background:
+                "assets/backgrounds/scene1.jpg",
+
+
+                speaker:
+                "",
+
+
+                text:
+                "Темрява поступово відступає.\n\nПеред тобою з'являється невідомий світ."
+
+
+
+            },
+
+
+
+
+            {
+
+
+                background:
+                "assets/backgrounds/scene1.jpg",
+
+
+                speaker:
+                "Героїня",
+
+
+                text:
+                "Де я?.."
+
+
+
+            },
+
+
+
+
+            {
+
+
+                background:
+                "assets/backgrounds/scene1.jpg",
+
+
+                speaker:
+                "",
+
+
+                text:
+                "Величезне місто майбутнього оживає навколо тебе."
+
+
+
+            }
+
+
+        ]
+
+
+    }
+
+
+};
