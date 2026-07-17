e815b538e68a7d8a59797cd9d5186ad56c7c4348
dimon540 <dmitrozdanevic22@gmail.com>
2026-07-17 14:04:31 +0300

    Implement settings menu with volume control

--- js/ui/settingsMenu.js
+++ js/ui/settingsMenu.js
@@ -1,1 +1,70 @@
+// =========================================
+// SETTINGS MENU
+// =========================================
 
+
+console.log("Settings Menu завантажено");
+
+
+
+
+
+function openSettingsMenu(){
+
+
+
+document.getElementById("app").innerHTML = `
+
+
+
+<div class="menu-screen">
+
+
+<h1>
+НАЛАШТУВАННЯ
+</h1>
+
+
+
+
+<label>
+
+Гучність музики
+
+</label>
+
+
+
+<input 
+type="range"
+min="0"
+max="1"
+step="0.1"
+oninput="AudioManager.setMusicVolume(this.value)"
+>
+
+
+
+
+
+<br><br>
+
+
+
+<button onclick="openMainMenu()">
+
+Назад
+
+</button>
+
+
+
+</div>
+
+
+
+`;
+
+
+
+}
