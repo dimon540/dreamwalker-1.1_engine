f71698123272b710d580ac0521c28e5540117202
dimon540 <dmitrozdanevic22@gmail.com>
2026-07-17 14:03:16 +0300

    Implement chapter menu UI in chapterMenu.js

--- js/ui/chapterMenu.js
+++ js/ui/chapterMenu.js
@@ -1,1 +1,88 @@
+// =========================================
+// DREAMWALKER CHAPTER MENU
+// =========================================
 
+
+console.log("Chapter Menu завантажено");
+
+
+
+
+function openChapterMenu(){
+
+
+
+const app =
+document.getElementById("app");
+
+
+
+app.innerHTML = `
+
+
+
+<div class="menu-screen">
+
+
+
+<h1>
+РОЗДІЛИ
+</h1>
+
+
+
+
+<div class="chapter-list">
+
+
+
+<button onclick="startScene('scene1')">
+
+Пролог:
+Невідоме місто
+
+</button>
+
+
+
+<button disabled>
+
+Глава 1
+(заблоковано)
+
+</button>
+
+
+
+<button disabled>
+
+Глава 2
+(заблоковано)
+
+</button>
+
+
+
+</div>
+
+
+
+
+
+<button onclick="openMainMenu()">
+
+Назад
+
+</button>
+
+
+
+</div>
+
+
+
+`;
+
+
+
+}
