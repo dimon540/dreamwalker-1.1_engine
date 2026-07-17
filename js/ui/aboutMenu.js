a809d2bf1276ea26d5fd40b955c7246af35b2d66
dimon540 <dmitrozdanevic22@gmail.com>
2026-07-17 14:04:07 +0300

    Implement About Menu with application details
    Added an 'About' menu with information about the application.

--- js/ui/aboutMenu.js
+++ js/ui/aboutMenu.js
@@ -1,1 +1,63 @@
+// =========================================
+// ABOUT
+// =========================================
 
+
+console.log("About Menu завантажено");
+
+
+
+
+
+function openAboutMenu(){
+
+
+
+document.getElementById("app").innerHTML = `
+
+
+
+<div class="menu-screen">
+
+
+
+<h1>
+DREAMWALKER
+</h1>
+
+
+
+<p>
+
+Візуальна новела про мрії,
+страхи та шлях до себе.
+
+</p>
+
+
+
+<p>
+
+Версія рушія: 1.0
+
+</p>
+
+
+
+<button onclick="openMainMenu()">
+
+Назад
+
+</button>
+
+
+
+</div>
+
+
+
+`;
+
+
+
+}
