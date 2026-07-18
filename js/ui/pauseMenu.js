74a46939f8ad5294a8cb1fb670de4dcf655bcee0
dimon540 <dmitrozdanevic22@gmail.com>
2026-07-19 01:50:00 +0300

    Implement pause menu with open and close functions

--- js/ui/pauseMenu.js
+++ js/ui/pauseMenu.js
@@ -1,1 +1,84 @@
+// =========================================
+// DREAMWALKER PAUSE MENU
+// =========================================
 
+
+console.log("Pause Menu завантажено");
+
+
+
+
+
+function openPauseMenu(){
+
+
+    const app = document.getElementById("app");
+
+
+    app.innerHTML += `
+
+
+    <div class="popup-overlay">
+
+
+        <div class="popup-window">
+
+
+            <h2>
+                МЕНЮ
+            </h2>
+
+
+
+            <button 
+            class="back-button"
+            onclick="closePauseMenu()">
+
+                Повернутися
+
+            </button>
+
+
+
+            <button 
+            class="back-button"
+            onclick="openMainMenu()">
+
+                Головне меню
+
+            </button>
+
+
+
+        </div>
+
+
+    </div>
+
+
+    `;
+
+
+}
+
+
+
+
+
+
+function closePauseMenu(){
+
+
+    const popup =
+
+    document.querySelector(".popup-overlay");
+
+
+    if(popup){
+
+        popup.remove();
+
+    }
+
+
+}
