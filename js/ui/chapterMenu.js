5ab84ec19e9497cb28a143bce35b8f426074d294
dimon540 <dmitrozdanevic22@gmail.com>
2026-07-18 16:34:13 +0300

    Refactor openChapterMenu function structure

--- js/ui/chapterMenu.js
+++ js/ui/chapterMenu.js
@@ -8,80 +8,186 @@ console.log("Chapter Menu завантажено");
 
 
 
+
+// =========================================
+// OPEN CHAPTER MENU
+// =========================================
+
+
 function openChapterMenu(){
 
 
 
-const app =
-document.getElementById("app");
+    const app = document.getElementById("app");
 
 
 
-app.innerHTML = `
+    app.innerHTML = `
 
 
 
-<div class="menu-screen">
-
-
-
-<h1>
-РОЗДІЛИ
-</h1>
-
-
-
-
-<div class="chapter-list">
-
-
-
-<button onclick="startScene('scene1')">
-
-Пролог:
-Невідоме місто
-
-</button>
-
-
-
-<button disabled>
-
-Глава 1
-(заблоковано)
-
-</button>
-
-
-
-<button disabled>
-
-Глава 2
-(заблоковано)
-
-</button>
-
-
-
-</div>
+    <div class="sub-menu chapter-screen">
 
 
 
 
 
-<button onclick="openMainMenu()">
+        <div class="background-title">
 
-Назад
+            DREAMWALKER
 
-</button>
-
-
-
-</div>
+        </div>
 
 
 
-`;
+
+
+
+        <h1 class="sub-title">
+
+            РОЗДІЛИ
+
+        </h1>
+
+
+
+
+
+
+
+        <div class="chapter-panel">
+
+
+
+
+
+            <button
+
+            class="chapter unlocked"
+
+            onclick="startScene('scene1')">
+
+
+
+                <div class="chapter-name">
+
+                    ✓ ПРОЛОГ
+
+                </div>
+
+
+
+                <div class="chapter-description">
+
+                    Незнайоме місто
+
+                </div>
+
+
+
+            </button>
+
+
+
+
+
+
+
+
+            <button
+
+            class="chapter locked">
+
+
+
+                <div class="chapter-name">
+
+                    🔒 ГЛАВА 1
+
+                </div>
+
+
+
+                <div class="chapter-description">
+
+                    Скоро
+
+                </div>
+
+
+
+            </button>
+
+
+
+
+
+
+
+
+            <button
+
+            class="chapter locked">
+
+
+
+                <div class="chapter-name">
+
+                    🔒 ГЛАВА 2
+
+                </div>
+
+
+
+                <div class="chapter-description">
+
+                    Скоро
+
+                </div>
+
+
+
+            </button>
+
+
+
+
+
+
+
+        </div>
+
+
+
+
+
+
+
+        <button
+
+        class="back-button"
+
+        onclick="openMainMenu()">
+
+
+
+            Назад
+
+
+
+        </button>
+
+
+
+
+
+
+
+    </div>
+
+
+
+    `;
 
 
 
